ab90ceff2bc15e249b1f0d81ebd1317c4f1d5bce
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 10:45:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ export default function Home() {
   return (
     <>     
       <div className="container">
-        <main>
-          <h1 className="text-4xl p-12 text-center">
+        <main className='text-center text-xl'>
+          <h1 className="text-6xl p-12">
             Welcome to <a href="https://nextjs.org">Champs Firm</a>
           </h1>
+          <p>
+            a Consultancy Firm for your business, in the african domain
+          </p>
         </main>
       </div>
     </>
