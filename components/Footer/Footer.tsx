ec9c4189ca56dd0cec3dd38943569330dc9662df
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 10:36:57 +0200

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 export default function header() {
     return (
-      <div>
-        <footer className="bg-neutral-500">
-            <h1>Champs Firm</h1>
-            <p>
-                a Consultancy Firm for your business, in the african domain
-            </p>
-        </footer>
-      </div>
+    <footer className="fixed bottom-0 w-full border-t">
+        <h1>Champs Firm</h1>
+        <p>
+            a Consultancy Firm for your business, in the african domain
+        </p>
+        <section
+        className="flex justify-between w-full border-t"
+        > Copyright 2023 </section>
+    </footer>
     )
   }
   
