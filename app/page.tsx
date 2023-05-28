96606e27a442f15e409054e16ec9d7396867f6ad
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 14:49:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-import Image from 'next/image'
-
-
 export default function Home() {
   return (
     <>     
-      <div className="w-full min-h-[60vh]">
+      <div className='w-full min-h-[60vh]'>
         <main className='text-xl text-center mx-24'>
-          <h1 className="text-6xl pt-32">
-            Welcome to <a href="#">Champs Firm</a>
+          <h1 className='text-6xl pt-32'>
+            Welcome to <a href='#'>Champs Firm</a>
           </h1>
           <div className='w-1/6 h-[1px] bg-[#84be1f] my-4 mx-auto'></div>
           <p>
-          We're so very excited to have you as a client and wanted to thank you from the bottom of our hearts for choosing us. Our mission is to deliver our clients the very best clients experience we can, so please know that we value your feedback.
+          We&apos;re so very excited to have you as a client and wanted to thank you from the bottom of our hearts for choosing us. Our mission is to deliver our clients the very best clients experience we can, so please know that we value your feedback.
           </p>
           <button className='bg-[#84be1f] my-4 rounded-md px-4 py-2'>Explore..</button>
         </main>
