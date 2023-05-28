4cf5d642a175f23eae891bc5d6cbacf89976321f
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 14:52:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
           </h1>
           <div className='w-1/6 h-[1px] bg-[#84be1f] my-4 mx-auto'></div>
           <p>
-          We&apos;re so very excited to have you as a client and wanted to thank you from the bottom of our hearts for choosing us. Our mission is to deliver our clients the very best clients experience we can, so please know that we value your feedback.
+          We are so very excited to have you as a client and wanted to thank you from the bottom of our hearts for choosing us. Our mission is to deliver our clients the very best clients experience we can, so please know that we value your feedback.
           </p>
-          <button className='bg-[#84be1f] my-4 rounded-md px-4 py-2'>Explore..</button>
+          <button className='bg-[#84be1f] my-4 rounded-md px-4 py-2'>Explore.</button>
         </main>
       </div>
      
