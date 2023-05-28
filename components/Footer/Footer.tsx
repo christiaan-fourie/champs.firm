dfc9ab7debdd5bcf40b73f203492e9a4a0a69e81
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 10:45:27 +0200

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,12 @@
 export default function header() {
     return (
-    <footer className="fixed bottom-0 w-full border-t">
-        <h1>Champs Firm</h1>
-        <p>
-            a Consultancy Firm for your business, in the african domain
-        </p>
-        <section
-        className="flex justify-between w-full border-t"
-        > Copyright 2023 </section>
-    </footer>
+        <footer className="fixed bottom-0 w-full border-t">
+            <h1>Champs Firm</h1>
+            <section
+                className="w-full border-t">
+                <p className="text-center">© Copyright 2023 | Champs Firm Build By Psytech</p>
+            </section>
+        </footer>
     )
-  }
+}
   
