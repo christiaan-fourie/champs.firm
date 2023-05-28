4c4f6ab28cd71cea3c439f0a54096fe0f182a9ec
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 14:46:13 +0200

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image"
+
 export default function header() {
   return (
     <div>
-      <header className="flex justify-between w-full px-12 border-b py-4">
-        <h1 className="text-2xl">Champs Firm</h1>
+      <header className="flex justify-between items-center w-full px-12 border-b py-4">
+        <h1 className="text-2xl"><Image alt="" src='/assets/logo.png' width={60} height={60} /></h1>
         <ul className="flex gap-6">
-          <li className="hover:bg-neutral-500">Home</li>
-          <li>About</li>
-          <li>Services</li>
+          <li className="hover:bg-[#84be1f] hover:text-black px-4 py-2 rounded-md">Home</li>
+          <li className="hover:bg-[#84be1f] hover:text-black px-4 py-2 rounded-md">About</li>
+          <li className="hover:bg-[#84be1f] hover:text-black px-4 py-2 rounded-md">Services</li>
         </ul>
     </header>
     </div>
