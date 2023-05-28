116bd12b1bea416a9131a3f6dc61c20f1d46c6bb
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-28 14:46:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
     <>     
-      <div className="container">
-        <main className='text-center text-xl'>
-          <h1 className="text-6xl p-12">
-            Welcome to <a href="https://nextjs.org">Champs Firm</a>
+      <div className="w-full min-h-[60vh]">
+        <main className='text-xl text-center mx-24'>
+          <h1 className="text-6xl pt-32">
+            Welcome to <a href="#">Champs Firm</a>
           </h1>
+          <div className='w-1/6 h-[1px] bg-[#84be1f] my-4 mx-auto'></div>
           <p>
-            a Consultancy Firm for your business, in the african domain
+          We're so very excited to have you as a client and wanted to thank you from the bottom of our hearts for choosing us. Our mission is to deliver our clients the very best clients experience we can, so please know that we value your feedback.
           </p>
+          <button className='bg-[#84be1f] my-4 rounded-md px-4 py-2'>Explore..</button>
         </main>
       </div>
+     
     </>
   )
 }
