699d59633af1449ef5434468096df2cd1b2fcd34
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-29 11:51:55 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
