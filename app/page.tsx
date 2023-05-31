88271e588b673358baf1a42e4af9f2cadc78dabf
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-05-31 09:05:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
       <div className='w-full min-h-[60vh] '>
         <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 -z-10'></div>
         <main className='text-xl text-center mx-24'>
-          <h1 className='text-6xl font-light pt-32'>
+          <h1 className='text-6xl font-extrabold pt-32 text-center bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%'>
             Welcome to Champs Firm.
           </h1>
-          <div className='w-1/6 h-[1px] bg-[#84be1f] my-4 mx-auto'></div>
+          <div className='w-1/6 h-[1px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-4 mx-auto'></div>
           <p>
           We are so very excited to have you as a client and wanted to thank you from the bottom of our hearts for choosing us. Our mission is to deliver our clients the very best clients experience we can, so please know that we value your feedback.
           </p>
