008784d3bb0a4796761bdac2f0b3cb1144e5974e
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-01 11:29:55 +0200

    Service Page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { FaHandsHelping } from 'react-icons/fa'
+
 const services = [
     {
         title: 'Registration and Compliance',
@@ -109,7 +111,7 @@ function servicesPage() {
   return (
     <div className='w-full min-h-[60vh] '>
         <main className='text-xl text-center mx-24'>
-            <h1 className='text-6xl font-extrabold pt-32 text-center bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%'>
+            <h1 className='md:text-6xl font-extrabold pt-32 text-center bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%'>
                 Our Services.
             </h1>
             <div className='w-1/6 h-[1px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-4 mx-auto'></div>
@@ -198,9 +200,60 @@ function servicesPage() {
             <li>Social Media Advertising</li>
             <li>Business Proposal</li>
             </ul> */}
-            <h1 className='my-12'>Content Coming Soon...</h1>
+
+            
 
         </main>
+        <div className='bg-black my-12 py-12'>
+            <div className='flex w-full items-center justify-center'>
+                <div className='hidden md:flex justify-center p-2 w-1/3'>
+                    <FaHandsHelping size={'50%'} className='' />
+                </div>
+                <div className='text-left w-1/2'>
+                    <div className='flex flex-col justify-start'>
+                        <FaHandsHelping size={'30%'} className='md:hidden p-4' />
+                        <h1 className='text-xl text-left lg:text-4xl'>Registration and Compliance</h1>
+                    </div>                    
+                    <div className='w-1/6 h-[1px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-4'></div>
+                    <p>Streamline your registration and compliance process with our comprehensive platform, ensuring legal adherence and efficient management of documents and deadlines.</p>
+                </div> 
+            </div>
+            <div className='pt-12'>
+                {/* Table with registrations and compliance services */}
+                <table className='min-w-[70%] mx-auto table-auto text-center bg-[#141b08]'>
+                    {/* <thead>
+                        <tr className='table-row'>
+                            <th className='table-cell'>Registration & Compliance</th>
+                            <th className='table-cell'>Compliance</th>
+                        </tr>
+                    </thead> */}
+                    <tbody>
+                        <tr className='table-row'>
+                            <td className='table-cell w-1/2'>Company Registration</td>
+                            <td className='table-cell'>Annual Returns</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td className='table-cell'>Non-Profit Company Registration</td>
+                            <td className='table-cell'>Financial Statements</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td className='table-cell'>Trust Registration</td>
+                            <td className='table-cell'>Income Tax Returns</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td className='table-cell'>Cooperative Registration</td>
+                            <td className='table-cell'>BBBEE Affidavits</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td className='table-cell'>Business Name Registration</td>
+                            <td className='table-cell'></td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+
     </div>
   )
 }
