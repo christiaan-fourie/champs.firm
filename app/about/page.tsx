3d143791f211be64abfe236a8701e1e019d6ca3a
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-12 14:27:27 +0200

    Update the about page
    Various updates to the about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,61 +1,106 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function About() {
   return (
-    <div className='w-full min-h-[60vh] '>
-        <main className='text-xl text-center md:mx-24'>
-          <h1 className='text-6xl font-extrabold pt-32 text-center bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%'>
-            Who we are.
-          </h1>
-          <div className='w-1/6 h-[1px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-4 mx-auto my-4 mx-auto'></div>
-          <p className="max-w- mx-auto">
-          Champs firm is a consultancy company whose aim, and dream is to serve SMMEs at all positions in the African domain.          
+    <div className="p-12 md:p-16">        
+        {/* Next Image */}
+        <div className="h-[60vh] w-full lg:w-1/2 fixed -z-30 -left-16 -translate-x-64 lg:-translate-x-0">
+          <Image
+            src="/assets/backgrounds/lionbackground.jpg"
+            alt="Hero Image"
+            layout="fill"
+            className="object-cover object-center invert rounded-full"
+          />
+        </div> 
+        <div className="text-right lg:w-1/2 ml-auto mt-8">
+          <div className=" text-white text-6xl lg:text-right font-extrabold text-center">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%">
+              Our Vision
+            </h1>
+          </div>
+          {/* A line */}
+          
+          <div className="w-1/2 h-[2px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-10 py-2/1 mx-auto lg:mr-0"></div>
+          <p className="glass p-4 rounded">
+          To become Africa&apos;s consultancy powerhouse in various
+            professional denominations.
+          We envision a future where African SMMEs are at the forefront of economic growth and innovation. By supporting and nurturing these businesses, we aim to contribute to the overall development and prosperity of the African continent.
           </p>
-          <div className="flex flex-col lg:flex-row  justify-around p-12">
-            <div className="hover:text-[#84be1f] border hover:border-[#84bd1f] rounded-lg p-12 w-full lg:w-1/3 lg:hover:w-1/2 transition-all duration-700">
-              <h2 className="text-4xl font-thin pb-6">Our Vision</h2>
-              <p>
-              To become Africa&apos;s consultancy powerhouse in various professional denominations.
-              </p>
-            </div>
-            <div className="hover:text-[#84be1f] border hover:border-[#84bd1f] rounded-lg p-12 w-full lg:w-1/3 lg:hover:w-1/2 transition-all duration-700">
-              <h2 className="text-4xl font-thin pb-6">Our Mission</h2>
-              <p>
-              To build creative brands, and companies that meets world class client/customer satisfaction.
-              </p>
-            </div>
-          </div>
-          <div>
-            <h2 className="text-4xl font-thin pb-6">Our Values</h2>
-            <div className="flex flex-col w-full xl:flex-row gap-6 items-center justify-around p-12">
-            {/* Our Values, Leadership, Exceptionalism, Accountability and Professionalism */}
-              <div className="hover:text-[#84be1f] border hover:border-[#84bd1f] rounded-lg p-12 w-full lg:w-1/3 lg:hover:w-1/2 transition-all duration-700">                
-                <h2 className="text-4xl font-thin pb-6">Leadership</h2>
-                <p>
-                We are leaders in our field, and we take on leadership roles in our community.
-                </p>
-              </div>
-              <div className="hover:text-[#84be1f] border hover:border-[#84bd1f] rounded-lg p-12 w-full lg:w-1/3 lg:hover:w-1/2 transition-all duration-700">
-                <h2 className="text-4xl font-thin pb-6">Exceptionalism</h2>
-                <p>
-                We are exceptional in our field, and we take on exceptional roles in our community.
-                </p>
-              </div>
-              <div className="hover:text-[#84be1f] border hover:border-[#84bd1f] rounded-lg p-12 w-full lg:w-1/3 lg:hover:w-1/2 transition-all duration-700">
-                <h2 className="text-4xl font-thin pb-6">Accountability</h2>
-                <p>
-                We are accountable in our field, and we take on accountability roles in our community.
-                </p>
-              </div>
-              <div className="hover:text-[#84be1f] border hover:border-[#84bd1f] rounded-lg p-12 w-full lg:w-1/3 lg:hover:w-1/2 transition-all duration-700">
-                <h2 className="text-4xl font-thin pb-6">Professionalism</h2>
-                <p>
-                We are professional in our field, and we take on professional roles in our community.
-                </p>
-              </div>
-              
+        </div>
+        {/* A Call to action button */}
+        <div className="flex justify-center lg:justify-end">
+          <Link href='/contact' className="bg-[#84be1f] hover:bg-[#1b6ec5] text-white font-bold py-2 px-4 rounded-full mt-12 transition-all duration-300">
+            Contact Us
+          </Link>
+        </div>
 
+        <div className="text-center lg:text-right lg:w-1/2 md:ml-auto mt-8">
+          <div className=" text-white text-6xl lg:text-right font-extrabold text-center">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%">
+              And Mission
+            </h1>
+          </div>
+          {/* A line */}          
+          <div className="w-1/2 h-[2px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-10 py-2/1 mx-auto lg:mr-0"></div>
+          <p>
+            Our mission is to provide comprehensive solutions and tailored support to SMMEs, enabling them to overcome challenges and unlock their full potential. We believe that by offering our expertise, guidance, and resources, we can make a significant impact on the growth and success of SMMEs throughout Africa.
+          </p>
+        </div>
+
+        {/* Our Values */}
+        <div className="text-center lg:text-right lg:w-1/2 md:ml-auto mt-8">
+          <div className=" text-white text-6xl lg:text-right font-extrabold text-center">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70%">
+              Our Values
+            </h1>
+          </div>  
+          {/* A line */}
+          <div className="w-1/2 h-[2px] bg-gradient-to-l from-[#84be1f] to-[#1b6ec5] from-30% to-70% my-10 py-2/1 mx-auto lg:mr-0"></div>
+          <p>
+            We are committed to providing our clients with the highest quality of service and support. Our four values are the foundation and pillars of our business and the driving force behind everything we do.
+          </p>
+          {/* Leadership
+              Exceptionalism
+              Accountability
+              Professionalism 
+          */}
+          {/* 4 Responsive cards that flex to a columb on small devices and two columbs on med devices */}
+          <div className="flex flex-col gap-4 mt-8">
+            <div className="glass p-4 rounded ">
+              <h3 className="text-2xl font-bold">Leadership</h3>
+              <p>
+                We are committed to providing our clients with the highest quality of service and support. Our four values are the foundation and pillars of our business and the driving force behind everything we do.
+              </p>
+            </div>
+            <div className="glass p-4 rounded">
+              <h3 className="text-2xl font-bold">Exceptionalism</h3>
+              <p>
+                We are committed to providing our clients with the highest quality of service and support. Our four values are the foundation and pillars of our business and the driving force behind everything we do.
+              </p>
+            </div>
+            <div className="glass p-4 rounded">
+              <h3 className="text-2xl font-bold">Accountability</h3>
+              <p>
+                We are committed to providing our clients with the highest quality of service and support. Our four values are the foundation and pillars of our business and the driving force behind everything we do.
+              </p>
+            </div>
+            <div className="glass p-4 rounded">
+              <h3 className="text-2xl font-bold">Professionalism</h3>
+              <p>
+                We are committed to providing our clients with the highest quality of service and support. Our four values are the foundation and pillars of our business and the driving force behind everything we do.
+              </p>
             </div>
           </div>
-        </main>
+
+          
+        </div>
+
+
+
+
+
+        
       </div>
-  )
+  );
 }
