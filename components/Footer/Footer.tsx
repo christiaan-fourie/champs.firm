b9538f4e04c13090fe6d986d849768f612a95546
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-12 12:46:28 +0200

    Update The Social Icon Links
    Now the social icons are working.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -16,10 +16,18 @@ export default function header() {
                     {littleLine()}
                     <p className='text-start max-w-md'>Champs firm is a consultancy company whose aim, and dream is to serve SMMEs at all positions in the African domain.</p>
                     <div className='flex justify-left gap-8 pt-8'>
-                        <FaFacebook size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500' />
-                        <FaTwitter size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500' />
-                        <FaInstagram size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500'  />
-                        <FaGithub size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500'  />
+                        <Link target='_blank' href='https://www.google.com/search?q=champs+firm+consulting+in+africa+facebook&oq=champs+firm+consulting+in+africa+facebook&aqs=chrome..69i57.359j0j1&sourceid=chrome&ie=UTF-8'>
+                            <FaFacebook size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500' />
+                        </Link>
+                        <Link target='_blank' href='https://twitter.com/champsfirm'>
+                            <FaTwitter size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500' />
+                        </Link>
+                        {/* <Link target='_blank' href=''>
+                            <FaInstagram size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500' />
+                        </Link> */}
+                        <Link target='_blank' href='https://github.com/christiaan-fourie/champs.firm'>
+                            <FaGithub size={35} className='hover:bg-[#84be1f] hover:text-black rounded-full p-1 transition-all duration-500' />
+                        </Link>
                     </div>
                 </div>  
                 <div className='p-12'>
@@ -63,7 +71,7 @@ export default function header() {
                 className="w-full border-t">
                 <p className="flex justify-between p-2 pl-6 items-center">
                     <span className="text-sm font-thin"> © Copyright 2023 Champs Firm | Dev with Psytech </span>
-                    <Link target='_blank' href='https://github.com/iam-krist/champs.firm'>
+                    <Link target='_blank' href='https://github.com/christiaan-fourie/champs.firm'>
                         <FaGithub size={30} className='p-1' title='Source Code'  />
                     </Link>
                 </p>
